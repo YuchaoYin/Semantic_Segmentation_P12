9ca7d0f894c32001bab3b82a20c270644685be8e
YuchaoYin <yinyuchao1990@hotmail.com>
2019-01-19 14:48:18 +0800

    updated load_vgg

--- main.py
+++ main.py
@@ -33,8 +33,18 @@ def load_vgg(sess, vgg_path):
     vgg_layer3_out_tensor_name = 'layer3_out:0'
     vgg_layer4_out_tensor_name = 'layer4_out:0'
     vgg_layer7_out_tensor_name = 'layer7_out:0'
+
+    tf.saved_model.loader.load(sess, [vgg_tag], vgg_path)
+    graph = tf.get_default_graph()
+    imageInput = graph.get_tensor_by_name(vgg_input_tensor_name)
+    keepProb = graph.get_tensor_by_name(vgg_keep_prob_tensor_name)
+    layer3Out = graph.get_tensor_by_name(vgg_layer3_out_tensor_name)
+    layer4Out = graph.get_tensor_by_name(vgg_layer4_out_tensor_name)
+    layer7Out = graph.get_tensor_by_name(vgg_layer7_out_tensor_name)
+
+
     
-    return None, None, None, None, None
+    return imageInput, keepProb, layer3Out, layer4Out, layer7Out
 tests.test_load_vgg(load_vgg, tf)
 
 
